e73586c3b8d2ecdb9f8e5cb1968df16a3b1c9f99
ElDainosor <eldainosor@gmail.com>
2018-06-17 02:12:21 +0000

    Switch to the original commit name
    maybe change it on the aokp commit on main branch

--- core/java/com/android/internal/util/crdroid/AwesomeAnimationHelper.java
+++ core/java/com/android/internal/util/crdroid/AwesomeAnimationHelper.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.android.internal.util.aospextended;
+package com.android.internal.util.crdroid;
 
 import android.content.Context;
 import android.content.res.Resources;
